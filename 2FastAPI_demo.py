aa26c646a2d0c12ce1121419c0d1f46b0c44359d
xie123 <3247017691@qq.com>
2026-08-10 15:29:04 +0800

    新建

---
+++ 2FastAPI_demo.py
@@ -0,0 +1,17 @@
+
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+@app.get("/users")
+def read_users():
+    return [
+        {"id": "1", "name": "John", "age": 20},
+        {"id": "2", "name": "Jane", "age": 25},
+        {"id": "3", "name": "Jim", "age": 30}
+    ]
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(app, host='0.0.0.0', port=8000, reload=False)
